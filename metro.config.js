9e580ddaa13d53fa92a062c1fdbd59d028f38f2e
Juanca Videla <juancavidela@MacBookPro.fibertel.com.ar>
2026-04-24 14:24:26 -0300

    fix: resolve path aliases in Metro bundler

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
@@ -6,6 +8,22 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const sourceRoot = path.resolve(__dirname, 'src');
+
+const config = {
+  resolver: {
+    resolveRequest: (context, moduleName, platform) => {
+      if (moduleName.startsWith('@/')) {
+        return context.resolveRequest(
+          context,
+          path.join(sourceRoot, moduleName.slice(2)),
+          platform,
+        );
+      }
+
+      return context.resolveRequest(context, moduleName, platform);
+    },
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
