93fae3c7635962b94c2fb69cbe9527db40db672f
Juanca Videla <juancavidela@MacBookPro.fibertel.com.ar>
2026-04-24 13:08:42 -0300

    chore: migrate tooling to yarn and stricter quality gates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,49 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native', 'prettier'],
+  plugins: ['import', 'jest', 'unused-imports'],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling', 'index'],
+          'type',
+        ],
+        pathGroups: [
+          {
+            pattern: '@/**',
+            group: 'internal',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+      },
+    ],
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'error',
+      {
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+        vars: 'all',
+        varsIgnorePattern: '^_',
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['**/*.test.ts', '**/*.test.tsx', '**/jest.setup.js'],
+      extends: ['plugin:jest/recommended', 'plugin:jest/style', 'prettier'],
+    },
+  ],
 };
---
+++ .github/pull_request_template.md
@@ -0,0 +1,15 @@
+## Summary
+
+- 
+
+## Testing
+
+- [ ] `yarn lint`
+- [ ] `yarn typecheck`
+- [ ] `yarn test`
+
+## Checklist
+
+- [ ] Branch is up to date with `main`
+- [ ] Scope is limited to one reviewable concern
+- [ ] README and docs were updated when setup or structure changed
